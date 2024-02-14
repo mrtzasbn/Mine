0ef1c6e1aa8999ef2aec391072442d6d0214d753
Morteza Asiyaban <morteza.asiyaban@tuwien.ac.at>
2024-02-14 12:48:37 +0100

    up

--- SHMP/Extract_Append_data_SHMP.py
+++ SHMP/Extract_Append_data_SHMP.py
@@ -1,7 +1,10 @@
 import re
 
-def extract_data_from_file(file_content):
-    data_blocks = re.findall(r'# Starttime:.*?# Scan finished at.*?(?=# Starttime:|$)', file_content, re.DOTALL)
+def extract_data_from_file(file_content, start_pattern, end_pattern=None):
+    if end_pattern:
+        data_blocks = re.findall(rf'{start_pattern}.*?{end_pattern}.*?(?={start_pattern}|$)', file_content, re.DOTALL)
+    else:
+        data_blocks = re.findall(rf'{start_pattern}.*?(?={start_pattern}|$)', file_content, re.DOTALL)
     return data_blocks
 
 def write_data_to_files(data_list, prefix='output'):
@@ -9,12 +12,14 @@ def write_data_to_files(data_list, prefix='output'):
         with open(f'{prefix}_{i+1}.dat', 'w') as f:
             f.write(data.strip())
 
-
-
 if __name__ == "__main__":
     filename = r'D:\upper_edge_fine_5K_infield_ramp.dat'
     with open(filename, 'r') as file:
         file_content = file.read()
 
-    extracted_data = extract_data_from_file(file_content)
+    # Define your custom start and end patterns
+    custom_start_pattern = '# Starttime'
+    custom_end_pattern = '# Scan finished at'
+
+    extracted_data = extract_data_from_file(file_content, custom_start_pattern)
     write_data_to_files(extracted_data)
