987f4b830235bbdce90a8c0d545d796a7566da70
Morteza Asiyaban <morteza.asiyaban@tuwien.ac.at>
2024-02-15 11:58:56 +0100

    up

--- SQUID/a = 124E-19.py
+++ SQUID/a = 124E-19.py
@@ -1,5 +1,12 @@
-a = 124E-9
+import math
 
-b =1E9* a *(1-(5/16)**4)**(-0.5)
 
-print(b*2*3)
+# Constants
+phi_0 = 2.067833848E-15  # Wb
+mu_0  = 1.25663706212E-6
+pi_value = math.pi
+lambda_value = 124E-9
+
+slope = (phi_0)/(8*pi_value*mu_0*(lambda_value)**2)
+
+print(slope)
--- SQUID/m-m+.py
+++ SQUID/m-m+.py
@@ -16,8 +16,8 @@ def read_squid_data(filename):
 
 
 # Data directory
-# file = r"D:\MyData\CERN\R183-5\SQUID\M(H)_loop_183_5_11K_HighFields.dc.dat"
-file = r"D:\MyData\CERN\R183-5\SQUID\M(H)_loop_183_5_5K_WholeLoop.dc.dat"
+file = r"D:\MyData\CERN\R183-5\SQUID\M(H)_loop_183_5_11K_HighFields.dc.dat"
+# file = r"D:\MyData\CERN\R183-5\SQUID\M(H)_loop_183_5_5K_WholeLoop.dc.dat"
 
 title= "Nb$_3$Sn Thin Film, Sample 183-5, 11K"
 
@@ -25,7 +25,9 @@ title= "Nb$_3$Sn Thin Film, Sample 183-5, 11K"
 a = 2275E-6
 b = 2143E-6
 d = 2.717E-6
-v = a *b* d
+d_cu = 495E-6
+v = a * b * d
+v_cu = a * b * d_cu
 
 
 # Interval for x-axis (Field values)
@@ -36,8 +38,9 @@ interval_end = 68000
 fig, ax = plt.subplots(figsize=(10, 8))
 
 df = read_squid_data(file).loc[:, ['Field (Oe)', 'Long Moment (emu)']]
-df['Field (Oe)'] = -df['Field (Oe)']
-df['Long Moment (emu)'] = -df['Long Moment (emu)']
+df['Field (Oe)'] = df['Field (Oe)']*1E-4
+# df['Field (Oe)'] = -df['Field (Oe)']
+# df['Long Moment (emu)'] = -df['Long Moment (emu)']
 
 df = df[(df['Field (Oe)'] >= interval_start) & (df['Field (Oe)'] <= interval_end)]
 df1 = df.groupby("Field (Oe)")['Long Moment (emu)'].apply(
@@ -88,6 +91,6 @@ plt.grid(True)
 plt.tight_layout()
 
 
-plt.savefig(title+'.pdf', format='pdf', bbox_inches='tight')
-plt.savefig(title+'.png', format='png', bbox_inches='tight')
+# plt.savefig(title+'.pdf', format='pdf', bbox_inches='tight')
+# plt.savefig(title+'.png', format='png', bbox_inches='tight')
 plt.show()
