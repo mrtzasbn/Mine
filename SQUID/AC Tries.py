f520e7f4c65e52f764d3a3fab2de43b9984cffb3
Morteza Asiyaban <morteza.asiyaban@tuwien.ac.at>
2024-03-14 08:40:28 +0100

    up

--- SQUID/AC Tries.py
+++ SQUID/AC Tries.py
@@ -17,8 +17,8 @@ def read_squid_data(filename):
 
 
 # List of file paths and legend labels
-file =   r"D:\MyData\IS258A\AC-77K_IS258A_High_80mT.ac.dat"
-# file02 = r"D:\MyData\IS258A\AC-5K_IS258A_High_10mT.ac.dat"
+file =   r"D:\MyData\IS258A\AC-5K_IS258A_High_80mT.ac.dat"
+file02 = r"D:\MyData\IS258A\AC-5K_IS258A_High_80mT_FC.ac.dat"
 # file03 = r"D:\MyData\IS258A\AC-77K_IS258A_High_80mT.ac.dat"
 
 title= "IS258, more AC"
@@ -28,10 +28,10 @@ df = read_squid_data(file).loc[:, ['Field (Oe)', "m' (emu)", 'm" (emu)', "Regres
 df.loc[df["Regression Fit"] <= 9.995E-1, ['m" (emu)']] = np.nan
 grouped = df.groupby("Field (Oe)")
 
-# df2 = read_squid_data(file02).loc[:, ['Field (Oe)', "m' (emu)", 'm" (emu)', "Regression Fit", "Time"]]
-# # df2 = df2[df2["Regression Fit"] > 9.999E-1]
-# df2.loc[df2["Regression Fit"] <= 9.995E-1, ['m" (emu)']] = np.nan
-# grouped02 = df2.groupby("Field (Oe)")
+df2 = read_squid_data(file02).loc[:, ['Field (Oe)', "m' (emu)", 'm" (emu)', "Regression Fit", "Time"]]
+# df2 = df2[df2["Regression Fit"] > 9.999E-1]
+df2.loc[df2["Regression Fit"] <= 9.995E-1, ['m" (emu)']] = np.nan
+grouped02 = df2.groupby("Field (Oe)")
 
 # df3 = read_squid_data(file03).loc[:, ['Field (Oe)', "m' (emu)", 'm" (emu)', "Regression Fit", "Time"]]
 # df3 = df3[df3["Regression Fit"] > 9.999E-1]
@@ -50,33 +50,33 @@ for name, group in grouped:
     min = 0
     # max = group['m" (emu)'].max()-min
     max = 1
-    ax.plot(group.reset_index(drop=True).index + 1, ((group.reset_index(drop=True)['m" (emu)']-min)/max)*1E6, label=f'Field {int(name)/10} (mT), 77K.', marker = 'o', color="black")
+    ax.plot(group.reset_index(drop=True).index + 1, ((group.reset_index(drop=True)['m" (emu)']-min)/max)*1E6, label=f'Field {int(name)/10} (mT).', marker = 'o', color="black")
 
-# for name02, group02 in grouped02:
+for name02, group02 in grouped02:
     
-#     # min = group02['m" (emu)'].min()
-#     min = 0
-#     # max = group02['m" (emu)'].max()-min
-#     max = 1 
-#     ax.plot(group02.reset_index(drop=True).index + 1, ((group02.reset_index(drop=True)['m" (emu)']-min)/max)*1E6, label=f'Field {int(name02)/10} (mT), 5K', marker = '*')
+    # min = group02['m" (emu)'].min()
+    min = 0
+    # max = group02['m" (emu)'].max()-min
+    max = 1 
+    ax.plot(group02.reset_index(drop=True).index + 1, ((group02.reset_index(drop=True)['m" (emu)']-min)/max)*1E6, label=f'Field {int(name02)/10} (mT), FC', marker = '*')
 
 
 
 
-# ax_inset = mpl_il.inset_axes(ax, width="40%", height="30%", loc='center right')
+ax_inset = mpl_il.inset_axes(ax, width="40%", height="30%", loc='center right')
 
-# for name02, group02 in grouped02:
+for name02, group02 in grouped02:
 
-#     # min = group03['m" (emu)'].min()
-#     min = 0
-#     # max = group03['m" (emu)'].max()-min
-#     max = 1
-#     ax_inset.plot(group02.reset_index(drop=True).index + 1, ((group02.reset_index(drop=True)['m" (emu)']-min)/max)*1E6, label=f'Field {int(name02)/10} (mT), 5K', marker = '+')
-# ax_inset.set_xlabel("Try")
-# ax_inset.set_ylabel('m" $\\times 10^{{{-6}}}$ (emu)')
-# ax_inset.tick_params(axis='y')
-# ax_inset.grid(True)
-# ax_inset.legend()
+    # min = group03['m" (emu)'].min()
+    min = 0
+    # max = group03['m" (emu)'].max()-min
+    max = 1
+    ax_inset.plot(group02.reset_index(drop=True).index + 1, ((group02.reset_index(drop=True)['m" (emu)']-min)/max)*1E6, label=f'Field {int(name02)/10} (mT), FC', marker = '+')
+ax_inset.set_xlabel("Try")
+ax_inset.set_ylabel('m" $\\times 10^{{{-6}}}$ (emu)')
+ax_inset.tick_params(axis='y')
+ax_inset.grid(True)
+ax_inset.legend()
 
 
 
@@ -110,8 +110,8 @@ for tick in ax.get_yticklabels():
 
 ax.grid(True)
 
-plt.savefig("AC_77K" + '.pdf', format='pdf', bbox_inches='tight')
-plt.savefig("AC_77K" + '.png', format='png', bbox_inches='tight')
+plt.savefig("AC_" + '.pdf', format='pdf', bbox_inches='tight')
+plt.savefig("AC_" + '.png', format='png', bbox_inches='tight')
 
 plt.show()
 
