e2a92436d2c4c937ec403c66e260958fe0116a61
Morteza Asiyaban <morteza.asiyaban@tuwien.ac.at>
2024-03-14 08:32:38 +0100

    up

--- SQUID/AC Tries.py
+++ SQUID/AC Tries.py
@@ -17,21 +17,21 @@ def read_squid_data(filename):
 
 
 # List of file paths and legend labels
-file =   r"D:\MyData\CERN\R169-5\SQUID\AC Modified\AC-5K_Field_R86-5_High_ModifiedMoreAC80mT.ac.dat"
-file02 = r"D:\MyData\CERN\R169-5\SQUID\AC Modified\AC-5K_Field_R86-5_High_ModifiedMoreAC80mT_move.ac.dat"
-# file03 = r"D:\MyData\CERN\R86-5\SQUID\AC Modified 02\AC-5K_Field_R86-5_High_ModifiedMoreAC80mT_AC.ac.dat"
+file =   r"D:\MyData\IS258A\AC-77K_IS258A_High_80mT.ac.dat"
+# file02 = r"D:\MyData\IS258A\AC-5K_IS258A_High_10mT.ac.dat"
+# file03 = r"D:\MyData\IS258A\AC-77K_IS258A_High_80mT.ac.dat"
 
-title= "Sample 86-5, more AC"
+title= "IS258, more AC"
 
 df = read_squid_data(file).loc[:, ['Field (Oe)', "m' (emu)", 'm" (emu)', "Regression Fit", "Time"]]
 # df = df[df["Regression Fit"] > 9.999E-1]
 df.loc[df["Regression Fit"] <= 9.995E-1, ['m" (emu)']] = np.nan
 grouped = df.groupby("Field (Oe)")
 
-df2 = read_squid_data(file02).loc[:, ['Field (Oe)', "m' (emu)", 'm" (emu)', "Regression Fit", "Time"]]
-# df2 = df2[df2["Regression Fit"] > 9.999E-1]
-df2.loc[df2["Regression Fit"] <= 9.995E-1, ['m" (emu)']] = np.nan
-grouped02 = df2.groupby("Field (Oe)")
+# df2 = read_squid_data(file02).loc[:, ['Field (Oe)', "m' (emu)", 'm" (emu)', "Regression Fit", "Time"]]
+# # df2 = df2[df2["Regression Fit"] > 9.999E-1]
+# df2.loc[df2["Regression Fit"] <= 9.995E-1, ['m" (emu)']] = np.nan
+# grouped02 = df2.groupby("Field (Oe)")
 
 # df3 = read_squid_data(file03).loc[:, ['Field (Oe)', "m' (emu)", 'm" (emu)', "Regression Fit", "Time"]]
 # df3 = df3[df3["Regression Fit"] > 9.999E-1]
@@ -46,27 +46,47 @@ fig, ax = plt.subplots(figsize=(10, 8))
 # Plot 'Value' against index for each group
 for name, group in grouped:
     
-    min = group['m" (emu)'].min()
-    # min = 0
-    max = group['m" (emu)'].max()-min
-    # max = 1
-    ax.plot(group.reset_index(drop=True).index + 1, ((group.reset_index(drop=True)['m" (emu)']-min)/max)*1, label=f'Field {int(name)/10} (mT)', marker = 'o', color="black")
+    # min = group['m" (emu)'].min()
+    min = 0
+    # max = group['m" (emu)'].max()-min
+    max = 1
+    ax.plot(group.reset_index(drop=True).index + 1, ((group.reset_index(drop=True)['m" (emu)']-min)/max)*1E6, label=f'Field {int(name)/10} (mT), 77K.', marker = 'o', color="black")
 
-for name02, group02 in grouped02:
+# for name02, group02 in grouped02:
     
-    min = group02['m" (emu)'].min()
-    # min = 0
-    max = group02['m" (emu)'].max()-min
-    # max = 1 
-    ax.plot(group02.reset_index(drop=True).index + 1, ((group02.reset_index(drop=True)['m" (emu)']-min)/max)*1, label=f'Field {int(name02)/10} (mT), Move', marker = '*')
+#     # min = group02['m" (emu)'].min()
+#     min = 0
+#     # max = group02['m" (emu)'].max()-min
+#     max = 1 
+#     ax.plot(group02.reset_index(drop=True).index + 1, ((group02.reset_index(drop=True)['m" (emu)']-min)/max)*1E6, label=f'Field {int(name02)/10} (mT), 5K', marker = '*')
 
-# for name03, group03 in grouped03:
-    
-#     min = group03['m" (emu)'].min()
-#     # min = 0
-#     max = group03['m" (emu)'].max()-min
-#     # max = 1
-#     ax.plot(group03.reset_index(drop=True).index + 1, (group03.reset_index(drop=True)['m" (emu)']-min)/max, label=f'Field {int(name03)/10} (mT), After DC', marker = '+')
+
+
+
+# ax_inset = mpl_il.inset_axes(ax, width="40%", height="30%", loc='center right')
+
+# for name02, group02 in grouped02:
+
+#     # min = group03['m" (emu)'].min()
+#     min = 0
+#     # max = group03['m" (emu)'].max()-min
+#     max = 1
+#     ax_inset.plot(group02.reset_index(drop=True).index + 1, ((group02.reset_index(drop=True)['m" (emu)']-min)/max)*1E6, label=f'Field {int(name02)/10} (mT), 5K', marker = '+')
+# ax_inset.set_xlabel("Try")
+# ax_inset.set_ylabel('m" $\\times 10^{{{-6}}}$ (emu)')
+# ax_inset.tick_params(axis='y')
+# ax_inset.grid(True)
+# ax_inset.legend()
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -76,8 +96,8 @@ legend_font = {'family': 'serif', 'size': 12, 'weight': 'regular'}
 
 ax.set_title('m" (emu) vs  AC tries, 5K', fontdict)
 ax.set_xlabel('Try', fontdict)
-# ax.set_ylabel('m" $\\times 10^{{{-6}}}$ (emu)', fontdict)
-ax.set_ylabel('m" (Normalized)', fontdict)
+ax.set_ylabel('m" $\\times 10^{{{-6}}}$ (emu)', fontdict)
+# ax.set_ylabel('m" (Normalized)', fontdict)
 ax.legend(prop=legend_font)
 
 # Set tick labels
@@ -90,8 +110,8 @@ for tick in ax.get_yticklabels():
 
 ax.grid(True)
 
-# plt.savefig("ACNormal" + '.pdf', format='pdf', bbox_inches='tight')
-# plt.savefig("ACNormal" + '.png', format='png', bbox_inches='tight')
+plt.savefig("AC_77K" + '.pdf', format='pdf', bbox_inches='tight')
+plt.savefig("AC_77K" + '.png', format='png', bbox_inches='tight')
 
 plt.show()
 
